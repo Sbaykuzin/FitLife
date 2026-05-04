702d58982d242f0ec83b36a56c895024516631de
Sergei Baykuzin <sbaykuzin@bk.ru>
2026-05-04 19:35:30 +0400

    изменение комментариев

--- fit_life.py
+++ fit_life.py
@@ -1,26 +1,24 @@
+# Проект FitLife - MVP версия 1.0
 import io
 import sys
 
 if sys.platform == 'win32':
     sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-# Проект FitLife - MVP версия 1.0
-# Создаем константы
+
+
 WATER = 30
 ML_TO_L = 1000
 
-# Принимаем имя пользователя
+
 name_user = input('Введите имя:')
 
 
-# Принимаем возраст пользователя
 user_age = int(input('Введите Ваш возраст:'))
 
 
-# Принимаем вес пользователя
 user_weight = float(input('Введите Ваш вес в кг.:'))
 
 
-# Принимаем рост пользователя
 user_height = float(input('Введите Ваш рост в метрах (пример 1.75):'))
 
 
@@ -32,7 +30,6 @@ bmi = round(user_weight / (user_height ** 2), 1)
 water_needed = round(user_weight * WATER / ML_TO_L, 1)
 
 
-# Вывод красивого результата
 print("=" * 50)
 print(f'Отчет для пользователя: {name_user}, возраст {user_age}')
 print(f'Твой индекс массы тела - {bmi}')
